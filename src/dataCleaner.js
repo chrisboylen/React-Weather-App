ddc69f657017113115dbd3b9a43cfb792cf2df24
Chris Boylen <billpolster@gmail.com>
2018-07-18 18:06:33 -0600

    Add displayWeather method to and bind method to app

--- src/dataCleaner.js
+++ src/dataCleaner.js
@@ -4,11 +4,11 @@ export const currentWeatherCleaned = (data) => {
   const currentWeatherCleaned = {
     city: data.current_observation.display_location.city,
     condition: data.current_observation.weather,
-    day: data.current_observation.forecast.simpleforecast.forecast.forecastday[0].weekday,
+    day: data.forecast.simpleforecast.forecastday[0].date.weekday,
     temp: data.current_observation.temp_f,
     high: data.forecast.simpleforecast.forecastday[0].high.fahrenheit,
     low: data.forecast.simpleforecast.forecastday[0].low.fahrenheit,
-    summary: data.forecast.simpleforecast.forecastday.fcttext
+    summary: data.forecast.txt_forecast.forecastday[0].fcttext
   }
   return currentWeatherCleaned;
 }
