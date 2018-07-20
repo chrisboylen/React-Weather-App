07bca37dcdcae6fbb57e2ae7c9d3dbdafd8ff18a
Chris Boylen <billpolster@gmail.com>
2018-07-20 16:54:47 -0600

    Add and pass test so that currentWeather exists

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    localStorage.clear();
+    wrapper = shallow(<App />);
+  })
+  
+  it('Should exist', () => {
+    expect(wrapper).toBeDefined();
+  })
+
+})
