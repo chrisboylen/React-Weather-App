1dc09e1fb804cd4690118768f427586b9572afa2
Chris Boylen <billpolster@gmail.com>
2018-07-20 17:55:59 -0600

    Add statefull component for search with an input value set to an empty string

---
+++ src/Search.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
