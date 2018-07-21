2dded642b45d899c9d0286456e53278f7e6dc984
Chris Boylen <billpolster@gmail.com>
2018-07-20 18:21:55 -0600

    Add form with input and submit button to render of search

--- src/Search.js
+++ src/Search.js
@@ -8,9 +8,22 @@ class Search extends Component {
     }
   }
 
+  updateLocation = (e) => {
+    this.setState({ input: e.target.value })
+  }
+
   render() {
     return (
-
+      <form onSubmit={ (e) => e.preventDefault() }>
+        <input
+          type="text"
+          placeholder = "Type city, state here!" 
+          onChange={ (e) => this.updateLocation(e) }
+          />
+        <button onClick={ (e) => this.props.getUserLocation(this.state.input) }>
+          Submit
+        </button>
+      </form>
     );
   }
 }
