b84a40d77cb9565eb34f997ef659b61b44310283
Chris Boylen <billpolster@gmail.com>
2018-07-23 17:30:24 -0600

    Add and pass test so that card component renders an hour component with the corresponding props

--- src/Card.test.js
+++ src/Card.test.js
@@ -34,6 +34,19 @@ describe('Card', () => {
     expect(wrapper.props().summary).toEqual('Sun and clouds mixed. High 51F. Winds NE at 10 to 15 mph.');
   })
 
+  it('should render an hour card with the correct props', () => {
+    let mockData = sevenHourCleaned(data);
+    let wrapper = mount(<Card 
+                          hour={ mockData[0].hour }
+                          src={ mockData[0].icon }
+                          temp={ mockData[0].temp }
+                        />);
+    expect(wrapper.find('.seven-hour').length).toEqual(1);
+    expect(wrapper.props().hour).toEqual('12:00 PM');
+    expect(wrapper.props().src).toEqual('http://icons.wxug.com/i/c/k/partlycloudy.gif');
+    expect(wrapper.props().temp).toEqual('47');
+  })
+
   it('should render a TenDay component with the correct props', () => {
     let mockData = tenDayCleaned(data);
     let wrapper = mount(<Card 
