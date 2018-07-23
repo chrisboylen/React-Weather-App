6afaf15c07f1451f29606f101cd311214f5ae6d5
Chris Boylen <billpolster@gmail.com>
2018-07-23 16:41:33 -0600

    Add and pass test so card component renders a current weather component with the correct props

--- src/Card.test.js
+++ src/Card.test.js
@@ -12,5 +12,26 @@ describe('Card', () => {
     expect(wrapper).toBeDefined()
   })
 
-  
+  it('should render a current weather card', () => {
+    let mockData = currentWeatherCleaned(data);
+    let wrapper = mount(<Card
+                          city = { mockData.city }
+                          condition = { mockData.condition }
+                          currentDay = { mockData.currentDay }
+                          temp = { mockData.temp }
+                          high = { mockData.high }
+                          low = { mockData.low }
+                          summary = { mockData.summary }
+                        />);
+      console.log(wrapper.debug());
+
+    expect(wrapper.find('.current-weather').length).toEqual(1);
+    expect(wrapper.props().city).toEqual('Louisville');
+    expect(wrapper.props().condition).toEqual('Mostly Cloudy');
+    expect(wrapper.props().currentDay).toEqual('Wednesday');
+    expect(wrapper.props().temp).toEqual(46);
+    expect(wrapper.props().high).toEqual('51');
+    expect(wrapper.props().low).toEqual('32');
+    expect(wrapper.props().summary).toEqual('Sun and clouds mixed. High 51F. Winds NE at 10 to 15 mph.');
+  })
 })
--- src/dataCleaner.js
+++ src/dataCleaner.js
@@ -4,7 +4,7 @@ export const currentWeatherCleaned = (data) => {
   const currentWeatherCleaned = {
     city: data.current_observation.display_location.city,
     condition: data.current_observation.weather,
-    day: data.forecast.simpleforecast.forecastday[0].date.weekday,
+    currentDay: data.forecast.simpleforecast.forecastday[0].date.weekday,
     temp: data.current_observation.temp_f,
     high: data.forecast.simpleforecast.forecastday[0].high.fahrenheit,
     low: data.forecast.simpleforecast.forecastday[0].low.fahrenheit,
