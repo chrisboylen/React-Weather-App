1e616445c92f67db59114c092146d003edcd109b
Chris Boylen <billpolster@gmail.com>
2018-07-24 13:12:15 -0600

    Add and pass test so that search component renders a form with an input field and submit button

--- src/Search.test.js
+++ src/Search.test.js
@@ -18,4 +18,14 @@ describe('Search', () => {
   it('should have a default state with an empty string of input', () => {
     expect(wrapper.state()).toEqual({ input: '' })
   })
+
+  it('should render a Search component that contains a form with and input field and a submit button', () => {
+    expect(wrapper.find('input').length).toEqual(1)
+    expect(wrapper.find('button').length).toEqual(1)
+    expect(wrapper.find('form').length).toEqual(1)
+  })
+
+  
+
+  it('should ')
 })
