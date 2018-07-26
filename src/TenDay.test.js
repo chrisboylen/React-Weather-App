6b4210d3eacbd2b6f7f39e727fde808759deb914
Chris Boylen <billpolster@gmail.com>
2018-07-26 17:23:20 -0600

    Add and pass test so that seven hour component renders the appropriate card components

--- src/TenDay.test.js
+++ src/TenDay.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import TenDay from './TenDay';
-import { currentWeatherCleaned, sevenHourCleaned, tenDayCleaned } from './dataCleaner';
+import { tenDayCleaned } from './dataCleaner';
 import data from './mockData';
 
 describe('TenDay', () => {
